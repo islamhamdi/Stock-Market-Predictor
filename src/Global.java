b62ff5a61db93144b2d7ede19f70d02c8d6fe30d
islamhamdi <islam.hamdy48@gmail.com>
2014-03-07 00:15:38 +0200

    Fixing Max_Diameter bug + Cleaning out GexfOutputGraph

---
+++ src/Global.java
@@ -0,0 +1,5 @@
+public class Global {
+	static String path = "/home/islamhamdi/Desktop/TwitterStockData";
+	static String StatFolderPath = "Statistics";
+	static String historyPath = "/home/islamhamdi/Dropbox/Stock Market Daily Data/historical prices/";
+}
