8ceb9c274f6606920192eb94e65cd7e3ba4ee12f
Mohammed-Gamal <eng.mgz@gmail.com>
2014-03-14 21:28:29 +0200

    add Tracking Companies

--- src/StockTwits/Main.java
+++ src/StockTwits/Main.java
@@ -1,18 +1,21 @@
 package StockTwits;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		GLOBAL.companies = new String[126];
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		GLOBAL.companies = new String[169];
 
-		BufferedReader br = new BufferedReader(new FileReader("TrackingCompanies.txt"));
-		for(int i = 0; i < GLOBAL.companies.length; i++){
+		BufferedReader br = new BufferedReader(new FileReader(
+				"TrackingCompanies.txt"));
+		for (int i = 0; i < GLOBAL.companies.length; i++) {
 			GLOBAL.companies[i] = br.readLine();
 		}
 		br.close();
-		
+
 		String directory = "./Data/";
 
 		CompanySeparator cs = new CompanySeparator();
