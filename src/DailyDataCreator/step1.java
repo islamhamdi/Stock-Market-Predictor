442bda4763eda4d6b23f977a83357db0d456be34
Mohammed-Gamal <eng.mgz@gmail.com>
2014-03-14 21:37:34 +0200

    add dailyConverter

---
+++ src/DailyDataCreator/step1.java
@@ -0,0 +1,101 @@
+package DailyDataCreator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import twitter4j.Status;
+
+public class step1 {
+	static String companies[];
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException {
+		Scanner sc = new Scanner(new File("companies.txt"));
+		int n = sc.nextInt();
+		companies = new String[n];
+		for (int i = 0; i < n; i++) {
+			companies[i] = sc.next();
+		}
+
+		String out = "./out1/";
+
+		File dir = new File(out);
+
+		if (!dir.exists())			dir.mkdir();
+		SeperateOnCompanies("./input/", out);
+	}
+
+	public static void SeperateOnCompanies(String Indir, String Outdir)
+			throws IOException, ClassNotFoundException {
+		int n = companies.length;
+		System.out.println("=> Start dividing tweets on Companies files");
+		// writer
+		FileOutputStream[] FIS = new FileOutputStream[n];
+		ObjectOutputStream[] OOS = new ObjectOutputStream[n];
+		for (int i = 0; i < OOS.length; i++) {
+			FIS[i] = new FileOutputStream(Outdir + companies[i]);
+			OOS[i] = new ObjectOutputStream(FIS[i]);
+		}
+
+		int[] counter = new int[n];
+		HashSet<Long> set = new HashSet<Long>();
+		int cnt = 0;
+		// input files
+		File inputDir = new File(Indir);
+		File[] files = inputDir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			set.clear();
+			cnt = 0;
+			if (files[i].isFile()) {
+				System.out.println("Load File : " + files[i].getName());
+
+				FileInputStream fr = new FileInputStream(
+						files[i].getAbsolutePath());
+				ObjectInputStream is = new ObjectInputStream(fr);
+				Status msg = (Status) is.readObject();
+
+				while (msg != null) {
+					cnt++;
+					long id = msg.getId();
+					String body = msg.getText();
+					if (!set.contains(id)) {
+						set.add(id);
+						for (int k = 0; k < n; k++) {
+							if (body.indexOf(companies[k]) >= 0) {
+								OOS[k].writeObject(msg);
+								counter[k]++;
+							}
+						}
+					}
+
+					try {
+						msg = (Status) is.readObject();
+					} catch (Exception e) {
+						fr.close();
+						is.close();
+						break;
+					}
+				}
+				System.out.println("total = " + cnt + " to " + set.size());
+			}
+
+		}
+		for (int i = 0; i < n; i++) {
+			OOS[i].flush();
+			FIS[i].flush();
+			OOS[i].close();
+			FIS[i].close();
+		}
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(companies[i] + "=" + counter[i]);
+		}
+		System.out.println("=> Finish dividing tweets on companies files\n");
+	}
+}
---
+++ src/DailyDataCreator/step2.java
@@ -0,0 +1,136 @@
+package DailyDataCreator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+import twitter4j.Status;
+
+public class step2 {
+
+	public step2() throws Exception {
+		String finaloutputDir = "S:\\Dropbox\\Stock Market Daily Data\\Twitter\\";
+		File dir = new File(finaloutputDir);
+		if (!dir.exists())
+			dir.mkdir();
+
+		String inputDir = "./out1/";
+		dir = new File(inputDir);
+
+		if (!dir.exists())
+			throw new Exception("input folder doesnt exist");
+
+		separateCompany(inputDir, finaloutputDir);
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		step2 s2 = new step2();
+	}
+
+	static void separateCompany(String inDir, String outDir) throws Exception {
+		HashMap<String, ObjectOutputStream> writer = new HashMap<String, ObjectOutputStream>();
+		Map<String, Integer> counter = new HashMap<String, Integer>();
+
+		// input files
+		File inputDir = new File(inDir);
+		File[] input = inputDir.listFiles();
+
+		for (int i = 0; i < input.length; i++) {
+			ArrayList<ObjectOutputStream> arrlst = new ArrayList<ObjectOutputStream>();
+			if (input[i].isFile()) {
+				// create company dir
+				String fullDir = outDir + "/" + input[i].getName();
+				File dir = new File(fullDir);
+				if (!dir.exists())
+					dir.mkdir();
+
+				System.out.println(input[i].getName());
+
+				// read input file
+				FileInputStream fr = new FileInputStream(
+						input[i].getAbsolutePath());
+				ObjectInputStream is = new ObjectInputStream(fr);
+
+				File companydir = new File(fullDir);
+				File[] allDays = companydir.listFiles();
+
+				// open files
+				for (int k = 0; k < allDays.length; k++) {
+					ObjectOutputStream oos;
+					if (allDays[k].isFile()) {
+						oos = new ObjectOutputStream(new FileOutputStream(
+								allDays[k].getAbsolutePath(), true));
+						arrlst.add(oos);
+						writer.put(allDays[k].getName(), oos);
+						counter.put(allDays[k].getName(), 0);
+					}
+				}
+				System.out.println("read file " + input[i].getName());
+				Status msg = (Status) is.readObject();
+
+				while (msg != null) {
+					Date d = msg.getCreatedAt();
+
+					DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+					formatter.setTimeZone(TimeZone
+							.getTimeZone("America/New_York"));
+
+					String name = formatter.format(d);
+
+					// write if ok
+					ObjectOutputStream oos;
+					if (writer.get(name) != null) {
+						oos = writer.get(name);
+						counter.put(name, counter.get(name) + 1);
+					} else {
+						counter.put(name, 1);
+						oos = new ObjectOutputStream(new FileOutputStream(
+								fullDir + "/" + name, true));
+						arrlst.add(oos);
+						writer.put(name, oos);
+					}
+					oos.writeObject(msg);
+
+					try {
+						msg = (Status) is.readObject();
+					} catch (Exception e) {
+						fr.close();
+						is.close();
+						break;
+					}
+				}
+				for (int j = 0; j < arrlst.size(); j++) {
+					arrlst.get(j).flush();
+					arrlst.get(j).close();
+				}
+
+				arrlst.clear();
+				writer.clear();
+				for (Map.Entry<String, Integer> entry : counter.entrySet()) {
+					System.out.println(entry.getKey() + " " + entry.getValue());
+				}
+				counter.clear();
+			}
+		}
+
+	}
+
+	static boolean isOK(String s, String thrshold) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyy");
+		Date from = sdf.parse(s);
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MM-yyy");
+		Date to = sdf2.parse(thrshold);
+		return from.after(to) || from.equals(to);
+	}
+
+}
