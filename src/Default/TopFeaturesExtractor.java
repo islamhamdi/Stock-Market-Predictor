fe5f6ec443978f1b7bebb54c5673e7a69b645054
mahmoud hanafy <mahmooud.hanafy@gmail.com>
2014-07-06 18:24:42 +0200

    get top 10 features & output result to excel file

--- src/Default/TopFeaturesExtractor.java
+++ src/Default/TopFeaturesExtractor.java
@@ -3,37 +3,125 @@ package Default;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Locale;
 
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.format.BorderLineStyle;
+import jxl.format.Colour;
 import jxl.read.biff.BiffException;
+import jxl.write.Border;
+import jxl.write.Label;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+import jxl.write.biff.RowsExceededException;
 
 public class TopFeaturesExtractor {
 	private static String inputDirectory = "/media/MyData/DropBox/Dropbox/Stock Market Daily Data/Average/normal_average.xls";
-	private static final int[] TABLE_START_COLUMN = { 0, 10, 20, 30 }, TABLE1_NUM_ROWS = { 19, 19, 171, 171 };
-	private static final int NUMBER_OF_COLUMNS = 7, NUMBER_OF_SHEETS = 6, TOP = 5;
+	private static String outputDirectory = "/media/MyData/DropBox/Dropbox/Stock Market Daily Data/Average/topFeatures.xls";
 
-	public static void main(String[] args) throws IndexOutOfBoundsException, BiffException, IOException {
+	private static final int[] VOLUME_TABLE_START_COLUMN = { 10, 30 };
+	private static final int[] PRICE_TABLE_START_COLUMN = { 0, 20 };
+	private static final int[] TABLE_NUM_ROWS = { 19, 171 };
+	private static final int NUMBER_OF_COLUMNS = 7, NUMBER_OF_SHEETS = 7, TOPN = 5;
+
+	public static void main(String[] args) throws IndexOutOfBoundsException, BiffException, IOException, WriteException {
 		Workbook normalWorkbook = getWorkBook(inputDirectory);
+		WritableWorkbook outputWorkbook = createNewWorkbook(outputDirectory);
 
 		for (int sheetIndex = 0; sheetIndex < NUMBER_OF_SHEETS; sheetIndex++) {
 			System.out.println(">> Sheet : " + sheetIndex);
-			for (int tableIndex = 0; tableIndex < TABLE_START_COLUMN.length; tableIndex++) {
-				System.out.println("Table : " + tableIndex);
-				
-				Sheet excelSheet = normalWorkbook.getSheet(sheetIndex);
-				Feature[] tableFeatures = readTable(excelSheet, TABLE_START_COLUMN[tableIndex], TABLE1_NUM_ROWS[tableIndex]);
-				Arrays.sort(tableFeatures);
-				
-				for(int i = 0; i < TOP; i++){
-					System.out.println(tableFeatures[i].toString());
-				}
-				
-				System.out.println();
-			}
+			Sheet normalSheet = normalWorkbook.getSheet(sheetIndex);
+
+			Feature[] top10FeaturesVolume = getTopNFeature(normalSheet, VOLUME_TABLE_START_COLUMN);
+			printFeatures("VOLUME FEATURES : ", top10FeaturesVolume);
+
+			Feature[] top10FeaturesPrice = getTopNFeature(normalSheet, PRICE_TABLE_START_COLUMN);
+			printFeatures("PRICE FEATURES : ", top10FeaturesPrice);
+
+			WritableSheet outputSheet = outputWorkbook.getSheet(sheetIndex);
+			outputSheet.getSettings().setDefaultColumnWidth(20);
+			writeWorkbookSheet(outputSheet, top10FeaturesPrice, "Price", 0);
+			writeWorkbookSheet(outputSheet, top10FeaturesVolume, "Volume", 10);
 		}
 
+		closeWorkBook(outputWorkbook);
+	}
+
+	private static void writeWorkbookSheet(WritableSheet sheet, Feature[] features, String title, int startColume)
+			throws RowsExceededException, WriteException {
+		for (int col = startColume + 1, index = -3; col < startColume + NUMBER_OF_COLUMNS + 1; col++, index++)
+			addLabel(sheet, col, 0, title + "(" + index + ")");
+
+		for (int row = 1; row <= features.length; row++) {
+			addLabel(sheet, startColume, row, features[row - 1].name);
+
+			for (int col = 1; col <= NUMBER_OF_COLUMNS; col++)
+				addNumber(sheet, col + startColume, row, features[row - 1].value[col - 1]);
+		}
+	}
+
+	private static void addNumber(WritableSheet sheet, int column, int row, double v) throws WriteException,
+			RowsExceededException {
+		WritableCellFormat cellFormat = new WritableCellFormat();
+		cellFormat.setBorder(Border.ALL, BorderLineStyle.THIN);
+		cellFormat.setWrap(true);
+		sheet.addCell(new jxl.write.Number(column, row, v, cellFormat));
+	}
+
+	private static void addLabel(WritableSheet sheet, int column, int row, String s) throws WriteException,
+			RowsExceededException {
+		WritableCellFormat cellFormat = new WritableCellFormat();
+		cellFormat.setBorder(Border.ALL, BorderLineStyle.THIN);
+		cellFormat.setWrap(true);
+		cellFormat.setBackground(Colour.GRAY_25);
+		sheet.addCell(new Label(column, row, s, cellFormat));
+	}
+
+	private static void closeWorkBook(WritableWorkbook workbook) throws IOException, WriteException {
+		workbook.write();
+		workbook.close();
+	}
+
+	private static WritableWorkbook createNewWorkbook(String directory) throws IOException {
+		File file = new File(directory);
+		WorkbookSettings wbSettings = new WorkbookSettings();
+		wbSettings.setLocale(new Locale("en", "EN"));
+		WritableWorkbook workbook = Workbook.createWorkbook(file, wbSettings);
+
+		String[] sheetname = { "Twitter", "StockTwits", "Combined", "Positive-Twitter", "Negative-Twitter",
+				"Positive-StockTwits", "Negative-StockTwits" };
+
+		for (int i = 0; i < sheetname.length; i++)
+			workbook.createSheet(sheetname[i], i);
+
+		return workbook;
+	}
+
+	private static void printFeatures(String title, Feature[] features) {
+		System.out.println(title);
+		for (int i = 0; i < features.length; i++)
+			System.out.println(features[i].toString());
+		System.out.println();
+	}
+
+	private static Feature[] getTopNFeature(Sheet excelSheet, int[] startCol) {
+		Feature[] topFeatures = new Feature[TOPN * 2];
+		for (int tableIndex = 0; tableIndex < VOLUME_TABLE_START_COLUMN.length; tableIndex++) {
+			Feature[] tableFeatures = readTable(excelSheet, startCol[tableIndex], TABLE_NUM_ROWS[tableIndex]);
+			Arrays.sort(tableFeatures);
+
+			for (int i = 0; i < TOPN; i++)
+				topFeatures[tableIndex * TOPN + i] = tableFeatures[i];
+		}
+
+		Arrays.sort(topFeatures);
+
+		return topFeatures;
 	}
 
 	private static Feature[] readTable(Sheet excelSheet, int startColumn, int numOfRows) {
@@ -76,7 +164,15 @@ public class TopFeaturesExtractor {
 
 		@Override
 		public String toString() {
-			return name + " " + Arrays.toString(value);
+			String formatedValues = format(value);
+			return name + formatedValues;
+		}
+
+		private String format(double[] value) {
+			String s = "";
+			for (int i = 0; i < value.length; i++)
+				s += " " + String.format("%.2f", value[i]);
+			return s;
 		}
 	}
 }
